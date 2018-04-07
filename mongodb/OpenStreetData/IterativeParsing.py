69283649486a5b7738713f5c4cdf1cff63d43818
Hemanth Kumar <mpshemanthkumar@gmail.com>
2018-04-07 12:53:51 +0530

    Update IterativeParsing.py

--- mongodb/OpenStreetData/IterativeParsing.py
+++ mongodb/OpenStreetData/IterativeParsing.py
@@ -1,1 +1,9 @@
-
+def count_tags(filename):
+        # YOUR CODE HERE
+    tags = {}
+    for event, elem in ET.iterparse(filename):
+        if elem.tag not in tags:
+            tags[elem.tag] = 1
+        else:
+            tags[elem.tag] += 1
+    return tags
